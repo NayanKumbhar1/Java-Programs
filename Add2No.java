a37b67b63e246c45372b6ab9e662cd9f519514c2
NayanKumbhar1 <nayankumbhar1515@gmail.com>
2022-10-19 09:46:05 +0530

    [ADDED]:Tahir Java Code for Additoin of 2 No

---
+++ Add2No.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Add2No
+{
+	public static void main(String args[])
+	{
+		int a,b,c=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the 2 Numbers:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("Addition="+c);
+	}
+}
